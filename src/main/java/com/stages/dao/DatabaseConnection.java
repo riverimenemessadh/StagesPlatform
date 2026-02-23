7daf37f3692928c8786d90cb3a43c421d84defd4
riverimenemessadh <sarahimenemessadh@gmail.com>
2026-02-23 13:31:45 +0100

    use env vars for database connection

--- src/main/java/com/stages/dao/DatabaseConnection.java
+++ src/main/java/com/stages/dao/DatabaseConnection.java
@@ -5,24 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/stages_db?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
-    
+
+    private static final String URL = System.getenv("DB_URL") != null
+            ? System.getenv("DB_URL")
+            : "jdbc:mysql://localhost:3306/stages_db?useSSL=false&serverTimezone=UTC";
+    private static final String USER = System.getenv("DB_USER") != null
+            ? System.getenv("DB_USER") : "root";
+    private static final String PASSWORD = System.getenv("DB_PASS") != null
+            ? System.getenv("DB_PASS") : "";
+
     static {
         try {
-            // Load MySQL JDBC Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             throw new RuntimeException("MySQL Driver not found", e);
         }
     }
-    
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-    
+
     public static void closeConnection(Connection conn) {
         if (conn != null) {
             try {
