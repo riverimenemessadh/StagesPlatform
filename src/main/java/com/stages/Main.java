6b43b1fcb2f4de9b732dcaf5ad38fbdf1e2e8d5a
riverimenemessadh <sarahimenemessadh@gmail.com>
2026-02-23 13:39:07 +0100

    fix Main.java for Docker deployment

--- src/main/java/com/stages/Main.java
+++ src/main/java/com/stages/Main.java
@@ -1,9 +1,6 @@
 package com.stages;
 
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 import java.io.File;
 
 public class Main {
@@ -16,17 +13,7 @@ public class Main {
         tomcat.getConnector();
 
         String webappDir = new File("src/main/webapp").getAbsolutePath();
-        var ctx = tomcat.addWebapp("", webappDir);
-
-        // Tell Tomcat where the compiled servlet classes are
-        WebResourceRoot resources = new StandardRoot(ctx);
-        resources.addPreResources(new DirResourceSet(
-            resources,
-            "/WEB-INF/classes",
-            new File("target/classes").getAbsolutePath(),
-            "/"
-        ));
-        ctx.setResources(resources);
+        tomcat.addWebapp("", webappDir);
 
         tomcat.start();
         tomcat.getServer().await();
